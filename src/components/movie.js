3bc953d9f33abaabe725137c04273819dec7533b
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-02 11:00:15 -0600

    Update movie.js

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
-import { fetchMovie } from "../actions/movieActions";
+import { fetchForecast } from "../actions/movieActions";
 import MovieDetail from "../components/moviedetail"
 
 // support routing
 
 function Movie(props) {
-    const [selectedMovie] = useState(props.selectedMovie);
+    const [selectedForecast] = useState(props.selectedForecast);
     const params = useParams();
-    const movieId = params.movieId;
-    console.log(movieId);
+    const forecastId = params.forecastId;
+    console.log(forecastId);
     const dispatch = useDispatch();
     if (selectedMovie == null) {
-        dispatch(fetchMovie(movieId));
+        dispatch(fetchForecast(forecastId));
     }
 
-    return (<MovieDetail movieId={movieId} />)
+    return (<MovieDetail forecastId={forecastId} />)
 }
 
-export default Movie;
+export default Forecast;
