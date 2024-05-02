a69ce65f74f5455d051b9fa2f76708bbeaf584ba
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-02 11:53:49 -0600

    Update store.js

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from "../reducers/authReducer";
-import movieReducer from "../reducers/movieReducer";
+import forecastReducer from "../reducers/movieReducer";
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 const store = createStore(
     combineReducers( {
         auth: authReducer,
-        movie: movieReducer
+        forecast: forecastReducer
     }),
     applyMiddleware(
         ...middlewares
