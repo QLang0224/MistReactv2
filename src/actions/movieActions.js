d7a200baa49e72a4afb4ca60cf93c78c63958c61
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-02 12:03:30 -0600

    Update movieActions.js

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -11,7 +11,7 @@ function forecastFetched(forecast) {
 
 export function setForecast(forecast) {
     return dispatch => {
-        dispatch(setForecast(forecast));
+        dispatch(forecastFetched(forecast));
     }
 }
 
