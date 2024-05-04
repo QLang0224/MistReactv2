116ac0935205200ee7fa45dbef2fdc43483fc231
ivanc1218 <ivan.contreras@ucdenver.edu>
2024-05-04 17:53:05 -0600

    updated forecastActions.js

--- src/actions/forecastActions.js
+++ src/actions/forecastActions.js
@@ -16,22 +16,37 @@ export function setForecast(forecast) {
 
 export function fetchForecast(forecastId) {
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/forecast/`, {
-            method: 'GET',
+        return fetch(`${env.REACT_APP_API_URL}/forecast/${forecastId}`, {
+            method: 'PUT',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Authorization': localStorage.getItem('token')
             },
-            mode: 'cors'
-        }).then((response) => {
+            mode: 'cors',
+        })
+        .then(response => {
             if (!response.ok) {
                 throw Error(response.statusText);
             }
-            return response.json()
-        }).then((res) => {
-            dispatch(forecastFetched(res.forecast));
-        }).catch((e) => console.log(e));
+
+            return fetch(`${env.REACT_APP_API_URL}/forecast/`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                },
+                mode: 'cors'
+            }).then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json()
+            }).then((res) => {
+                dispatch(forecastFetched(res.forecast));
+            }).catch((e) => console.log(e));
+        })
     }
 }
 
