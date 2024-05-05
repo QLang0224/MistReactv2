35f4cdb827dfefcd9bc66e3e5aeab04939268509
ivanc1218 <ivan.contreras@ucdenver.edu>
2024-05-04 23:28:19 -0600

    forecastlist.js updated

--- src/components/forecastlist.js
+++ src/components/forecastlist.js
@@ -43,7 +43,7 @@ class ForecastList extends Component {
                                 </LinkContainer>
                             </div>
                             <Carousel.Caption>
-                                <h3>{`${forecast.timeOfDay} - ${forecast.temperatureFahrenheit}°F`}</h3>
+                                <h3>{`${forecast.timeOfDay} - ${forecast.tempertureFarenheit}°F`}</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
                     )}
